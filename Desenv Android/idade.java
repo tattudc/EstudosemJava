59e88af8ad064a3be3235277b14970cf8d6b1cb6
Tarcisio Câmara <41845115+tattudc@users.noreply.github.com>
2019-03-23 20:37:29 -0300

    Primeiros programas em JAVA
    E criação da pasta do primeiro curso

---
+++ Desenv Android/idade.java
@@ -0,0 +1,14 @@
+public class Program {
+    public static void main(String[] args) {
+        int age = 17;
+        if(age > 0) {
+            if(age > 16) {
+                System.out.println("Welcome!");
+            } else {
+                System.out.println("Too Young");
+            }
+        } else {
+            System.out.println("Error");
+        }
+    }
+}
