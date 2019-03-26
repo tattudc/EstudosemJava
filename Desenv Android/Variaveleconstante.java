52d4ccdb8ffae06b2352c3ed296fbece46d2e180
Tarcisio Câmara <41845115+tattudc@users.noreply.github.com>
2019-03-26 05:33:12 -0300

    variaveis
    como criar variaveis e constantes em java e seus tipos

---
+++ Desenv Android/Variaveleconstante.java
@@ -0,0 +1,14 @@
+public class Hello{
+	public static void main(String[] args){
+		
+		String nome = "Tarcisio Dantas";
+		int numero = 10;
+		int numero2 = 5;
+		final int ALTURA = 187;
+		
+		System.out.println(nome);
+		System.out.println(ALTURA);
+		System.out.println(numero + numero2);
+		
+	}
+}
---
+++ Desenv Android/tiposdevariaveis.java
@@ -0,0 +1,39 @@
+public class Program
+{
+	public static void main(String[] args) {
+	    //Esse é um comentário de uma linha
+		//Variaveis inteira
+		int numero;
+		numero = 10;
+		int numero2 = 12;
+		//Strings são nomes e caracteres
+		String minhaVariavelString = "Tarcisio Câmara";
+		//byte são variaveis que vai de -127 a 128
+		byte minhaVariavelByte = 20;
+		//short vai de -32768 a 32768
+		short minhaVariavelShort = 3200;
+		//int vai de -2147483648 a 2147483648
+		int minhaVariavelInt = 33000;
+		//long vai de numeros bem grandes
+		long minhaVariavelLong = 10000000000000L;
+		//float usa decimais
+		float preco = 2.50f;
+		//double da precisao acima das 7 casas decimais
+		double preco2 = 2.1231231231;
+		//boolean é estados de true e false
+		boolean minhaVariavelBoolean = true;
+		
+		//Comando system.out.println é para printar algo na tela
+	    System.out.println(numero);
+		System.out.println(numero2);
+		System.out.println(minhaVariavelByte);
+		System.out.println(minhaVariavelShort);
+		System.out.println(minhaVariavelInt);
+		System.out.println(minhaVariavelLong);
+		System.out.println(preco);
+		System.out.println(preco2);
+		System.out.println(minhaVariavelBoolean);
+		System.out.println(minhaVariavelString);
+		
+	}
+}
